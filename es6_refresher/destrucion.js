ddc3224557c144db38217edb912181d883fddd9f
Ashik <ashiquejava@gmail.com>
2018-06-13 20:54:50 +0600

    Destructuring Arrays and Objects

---
+++ es6_refresher/destrucion.js
@@ -0,0 +1,13 @@
+// Destructuring: Easily extract array elements or object properties and store them in variables
+
+// Destructuring Array
+const [num1, num2] = [5, 6];
+console.log(num1);
+console.log(num2);
+const [x, , z] = [1, 2, 3];
+console.log(z);
+console.log(x);
+
+// Destructuring Objects
+const { name } = { name: 'Ashik', age: 23 };
+console.log(name);
